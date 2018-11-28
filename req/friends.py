eb8ea1f16f71ac54b7cdbd4d76aa9e5ba7377408
Andreyc4d <sbtr@mail.ru>
2018-11-28 11:36:33 +0100

    Allready done. HAND

--- req/friends.py
+++ req/friends.py
@@ -3,30 +3,35 @@ import sys
 def calc_age(uid):
     url_need = 'https://api.vk.com/method/'
     vk_method_need = 'friends.get'
+    if not uid.isdigit():
+        payload = {'v'              :'5.71', 
+                   'access_token'   :'17da724517da724517da72458517b8abce117da17da72454d235c274f1a2be5f45ee711',
+                   'user_ids'       :uid,
+                   'order'          :'name',
+                  }
+        r = requests.get(url_need+'users.get',payload)
+        try:
+            tmp = r.json()
+            tmp = tmp['response'][0]
+            uid = tmp['id']
+        except JSONDecodeError:
+            print('No data in json screen_name')
+
+            
     payload = {'v'              :'5.71', 
                'access_token'   :'17da724517da724517da72458517b8abce117da17da72454d235c274f1a2be5f45ee711',
                'user_id'        :uid,
                'order'          :'name',
                'fields'         :'bdate',
-               }
-    return requests.get(url_need+vk_method_need,params=payload)
-    
-print(__name__)
-
-if __name__ == '__main__':
-    try:
-        user_id = str(sys.argv[1])
-    except IndexError:
-        user_id = '12254363'
-    res = calc_age(user_id)
+              }
+    res = requests.get(url_need+vk_method_need,params=payload)
     try:
         j = res.json()
     except JSONDecodeError:
-        print('net Dannyh')
-
+        print('No data in json friens.get')
+                                                                              
     lst = dict()
-
-
+                                                                              
     for i in j['response']['items']:
         id = i['id']
         first_name = i['first_name']
@@ -37,12 +42,21 @@ if __name__ == '__main__':
             bdate  = 'qqqqq'
         if bdate.count('.') == 2:
             age  = 2018 - int(bdate[len(bdate)-4:])
-            #if age >= 39:
-            #print(first_name+' ' + last_name+ ' : '+str(age))
+            #if age >= 100:
+            #    print(first_name+' ' + last_name+ ' : '+str(age)+': ' + str(bdate))
             #try:
                 #lst[age] = lst[age]+1
             #except KeyError:
                 #lst[age] = 1
             lst[age] = lst.setdefault(age,0) + 1 #replace 4 string upper
         #print(str(first_name)+';'+str(last_name)+';'+str(id)+';'+str(bdate))
-    print(lst)
+    return lst
+    
+
+if __name__ == '__main__':
+    try:
+        user_id = str(sys.argv[1])
+    except IndexError:
+        user_id = '12254363'
+    res = calc_age(user_id)
+    print(res)
